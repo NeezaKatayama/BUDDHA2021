0a12f99738c6062da74c57e532d192422120417e
Neeza <yoshito0505@icloud.com>
2021-01-13 21:40:10 +0900

    Initial Commit

---
+++ BUDDHA4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BUDDHA4
+//
+//  Created by 片山義仁 on 2021/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
